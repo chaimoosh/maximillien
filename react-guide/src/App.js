0eb534e49d9b9dc98796e474e8ef3d0a7160fa3d
Chaim Reisman <crreisman@gmail.com>
2018-12-12 11:13:17 -0500

    more

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: "Max", age: 26},
+      {name: "Manu", age: 28},
+      {name: "Stephanie", age: 25}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
+        <button>Switch Name</button>
         < Person name="Chaim" age="26"/>
         < Person name="Raizy" age="24"/>
         < Person name="Dave" age="3" > I like tools </Person>
