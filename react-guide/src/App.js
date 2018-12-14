3d2bb2c64269cd1fcf513f6b8c3e7330147557df
Chaim Reisman <crreisman@gmail.com>
2018-12-13 19:28:05 -0500

    more

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Clicked!!')
     this.setState({ 
       person: [
-        {name: "Maxy", age: 26},
+        {name: newName, age: 26},
         {name: "Manu", age: 28},
         {name: "Stephanie", age: 24543}
       ]
@@ -27,10 +27,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        < Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        < Person name={this.state.person[1].name} age={this.state.person[1].age}/>
-        < Person name={this.state.person[2].name} age={this.state.person[2].age} click={this.switchNameHandler}> I like tools </Person>
+        <button onClick={() => this.switchNameHandler('Fatso')}>Switch Name</button>
+        < Person 
+         name={this.state.person[0].name}
+         age={this.state.person[0].age}/>
+        < Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age}
+          click={this.switchNameHandler.bind(this, 'Guatemala')}/>
+        < Person 
+          name={this.state.person[2].name} 
+          age={this.state.person[2].age} 
+          click={this.switchNameHandler.bind(this, 'Carla')}> I like tools </Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'duuuude'))
