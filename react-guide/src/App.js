1a0046ff49072e6a125e66e2d2fb913db2f3c652
Chaim Reisman <crreisman@gmail.com>
2019-01-03 10:44:14 -0500

    more

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -6,10 +6,12 @@ class App extends Component {
 
   state = {
     persons: [
-      {id: "345", name: "Max", age: 26},
-      {id: "345345", name: "Manu", age: 28},
-      {id: "4353452", name: "Stephanie", age: 25}
-    ]
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
   deletePersonHandler = (personIndex) => {
@@ -18,18 +20,23 @@ class App extends Component {
     this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: "Max", age: 26},
-        {name: event.target.value, age: 28},
-        {name: "Stephanie", age: 25}
-      ] 
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons
+    const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow})
   }
 
@@ -50,10 +57,11 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return <Person
-              onClick={this.deletePersonHandler(index)} 
+              click={() => this.deletePersonHandler(index)} 
               name={person.name} 
               age={person.age} 
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       )
