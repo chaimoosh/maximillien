f88d6c07afffa719fa21f30db6389b1da4d3d4e4
Chaim Reisman <crreisman@gmail.com>
2019-01-16 10:27:46 -0500

    more

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
-
+import {StyleRoot} from 'radium';
 
 class App extends Component {
 
@@ -88,14 +88,16 @@ class App extends Component {
       classes.push('bold')
     }
     return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-         style={style}
-         onClick={this.togglePersonsHandler}>Toggle Persons</button>
-         {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'duuuude'))
   }
